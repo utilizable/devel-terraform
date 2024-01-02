f47b5043bb223946fcaed2c60863b3913bd18d35
Igor <33967454+utilizable@users.noreply.github.com>
2024-01-02 23:36:31 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,12 +3,12 @@ module "module-a" {
   source = "./modules/lxd" 
 
   // pass provider variables to module 
-  provider_lxd_name     = var.provider_lxd_name 
-  provider_lxd_scheme   = var.provider_lxd_scheme 
-  provider_lxd_address  = var.provider_lxd_address 
+  provider_lxd_name = var.provider_lxd_name 
+  provider_lxd_scheme = var.provider_lxd_scheme 
+  provider_lxd_address = var.provider_lxd_address 
   provider_lxd_password = var.provider_lxd.password 
 
   // pass variables to module
-  container_name  = "test"
-  container_img   = "ubuntu" 
+  container_name = "test"
+  container_img = "ubuntu" 
 }
